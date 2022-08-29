9e3185ee26a73e0ebc2a743aa4897e9e157863d1
Andrian17 <andriancimen123@gmail.com>
2022-08-29 15:15:31 +0800

    perbaikan2

--- lib/src/makanan_screen.dart
+++ lib/src/makanan_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pizzahut_submission/src/detail_menu.dart';
 import 'package:pizzahut_submission/model/pizza.dart';
